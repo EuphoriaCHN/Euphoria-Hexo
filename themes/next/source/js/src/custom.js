fd2b31a25f25f05cbca3e02e5c0714d10266459b
EuphoriaCHN <879969355@qq.com>
2020-01-16 00:43:09 +0800

    Source

---
+++ themes/next/source/js/src/custom.js
@@ -0,0 +1,8 @@
+/* 轮播背景图片 */
+$(function () {
+    $.backstretch([
+        "/images/background/bg1.jpg",
+        "/images/background/bg2.jpg",
+        "/images/background/bg3.jpg",
+    ], {duration: 300000, fade: 1500});
+}); /*这个是背景轮播图，现在暂时可以不用输入*/
